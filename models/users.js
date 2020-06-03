415e70a2c30663721d000dca9ee68f705b3af155
GLR-901 <gabriel.l.romero@gmail.com>
2020-06-03 14:33:24 -0400

    Schema updated and second controller removed.

--- models/users.js
+++ models/users.js
@@ -3,11 +3,12 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const userSchema = Schema ({
-    username: String,
+    name: String,
     email: String,
     password: String,
-    loggedIn: { type:Boolean, default:false }
-}, { timestamps: true });
+    dob: Date,
+    loggedIn: { type:Boolean, default:false },
+},{ timestamps: true });
 
 const Users = mongoose.model('User', userSchema)
 
