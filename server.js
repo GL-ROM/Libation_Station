dd0d21fc8194a455fe4a4ce24fedd7007aba7e95
GLR-901 <gabriel.l.romero@gmail.com>
2020-06-02 21:25:34 -0400

    sign up form created (needs to be tested with route)

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json()); //use .json(), not .urlencoded()
 app.use(express.static('public')) // we need to tell express to use the public directory for static files... this way our app will find index.html as the route of the application! We can then attach React to that file!
 
 // Connect to mongo
-mongoose.connect(mongoURI, { useNewUrlParser: true }, () => {
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
     console.log("MongoDB connection made!", mongoURI)
 });
 
