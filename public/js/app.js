097d912eb850acaa45bc3618cae3b40731de3585
Zach <zachery.goodrich@gmail.com>
2020-06-03 16:00:36 -0700

    small change

--- public/js/app.js
+++ public/js/app.js
@@ -24,7 +24,7 @@ class SignUpForm extends React.Component {
             headers: {
                 'Accept': 'application/json, text/plain, */*',
                 'Content-Type': 'application/json'
-              }
+            }
         }).then(() => {
             console.log('Data was sent');
         })
@@ -168,9 +168,7 @@ class Test extends React.Component {
     render () {
         return (
             <h1>{this.props.var}</h1>
-
-bxc;d
-'dc'            )
+        )
     }
 }
 
